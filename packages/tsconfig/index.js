9bbf113b0b3327d7c6c0ded204f39449aaedf9ab
advl <sonora@779.mx>
2023-11-12 23:22:45 +0000

    TS Config

---
+++ packages/tsconfig/index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['!.storybook/*'],
+  env:{
+    browser:true,
+    node   :true,
+  },
+  extends  :['airbnb'],
+  rules:{
+    semi:[
+      2,
+      'never',
+    ],
+    'key-spacing':[
+      'error', {
+        multiLine:{
+          beforeColon:false,
+          afterColon :false,
+        },
+        align:{
+          beforeColon:false,
+          afterColon :false,
+          on         :'colon',
+        },
+      },
+    ],
+  }
+}
