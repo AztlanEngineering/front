261a19e61c1fcbd77a8cb37db2ae90c4b06754d2
advl <sonora@779.mx>
2023-11-14 20:57:15 +0000

    CI

--- config/eslint/index.js
+++ config/eslint/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends       :['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb'],
+  extends       :['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'airbnb', 'plugin:storybook/recommended'],
   parser        :'@typescript-eslint/parser',
   plugins       :['@typescript-eslint'],
   root          :true,
