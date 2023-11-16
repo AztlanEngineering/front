d475e10edad5aedd53704143f5c71881bb9e1da2
advl <sonora@779.mx>
2023-11-16 01:45:09 +0000

    All working

--- apps/template-react/src/ui/Tester/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Tester = void 0;
-/* @aztlan/generator-front 0.0.22 */
-var Tester_1 = require("./Tester");
-Object.defineProperty(exports, "Tester", { enumerable: true, get: function () { return __importDefault(Tester_1).default; } });
--- apps/template-react/src/ui/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QueryTester = exports.Tester = void 0;
-var Tester_1 = require("./Tester");
-Object.defineProperty(exports, "Tester", { enumerable: true, get: function () { return Tester_1.Tester; } });
-var QueryTester_1 = require("./QueryTester");
-Object.defineProperty(exports, "QueryTester", { enumerable: true, get: function () { return QueryTester_1.QueryTester; } });
