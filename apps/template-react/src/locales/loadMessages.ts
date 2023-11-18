541c348e3eb2351fd1a1dd2c6fbe7d631b49a591
advl <sonora@779.mx>
2023-11-18 20:08:10 +0000

    Test of prefetching locales

--- apps/template-react/src/locales/loadMessages.ts
+++ apps/template-react/src/locales/loadMessages.ts
@@ -1,9 +1,8 @@
-
 export default function loadMessages(locale: string) {
   switch (locale) {
     case 'es':
-      return import('./es.json')
+      return import(/* webpackPrefetch: true */ './es.json')
     default:
-      return import('./en.json')
+      return import(/* webpackPrefetch: true */ './en.json')
   }
 }
