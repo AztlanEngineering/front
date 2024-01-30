ec10418451f4e888f7c4393095b19a558be1bc5a
advl <700@779.mx>
2024-01-30 22:19:18 +0100

    CI test

--- apps/template-react/src/modules/base.homepage/pages/Home.tsx
+++ apps/template-react/src/modules/base.homepage/pages/Home.tsx
@@ -34,7 +34,7 @@ function Home() {
       </React.Suspense>
       <p className="container">
         {formatMessage(
-          m.welcome, { name: 'pg4' },
+          m.welcome, { name: 'pg5.2' },
         )}
       </p>
     </Template>
