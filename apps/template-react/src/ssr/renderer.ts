3e2d32684ab8759cfee2096bc0e77ee46f1ce120
advl <700@779.mx>
2024-01-31 11:45:52 +0100

    new build

--- apps/template-react/src/ssr/renderer.ts
+++ apps/template-react/src/ssr/renderer.ts
@@ -1,11 +1,39 @@
-import getServerEnvironment from '@aztlan/react-relay/server'
 import { JSXRenderer } from '@aztlan/react-ssr'
+import {
+  Environment, RecordSource, Store, Network,
+} from 'relay-runtime'
 import Base from './Base.tsx'
 // @ts-ignore
 import template from '../assets/html/index.html'
 import loadMessages from '../locales/loadMessages.ts'
 // @ts-ignore
 import stats from '../../public/loadable-stats.json'
+
+// Alternative way to do SSR
+// const collector = []
+const store = new Store(new RecordSource())
+
+// { collector }
+
+const getEnvironment = (url) => new Environment({
+  network:Network.create((
+    params, variables,
+  ) => {
+    const request = fetch(
+      url, {
+        method :'POST',
+        headers:{ 'Content-Type': 'application/json' },
+        body   :JSON.stringify({
+          query:params.text,
+          variables,
+        }),
+      },
+    ).then((response) => response.json())
+    // collector.push(request)
+    return request
+  }),
+  store,
+})
 
 /*
 import styleNames from '@aztlan/bem'
@@ -16,7 +44,7 @@ properly configured with CSS Modules', styleNames, styleNames.base)
 
 const renderer = new JSXRenderer(
   Base, template, stats, {
-    getEnvironment    :getServerEnvironment,
+    getEnvironment,
     defaultLocale     :'en',
     loadMessages,
     graphqlEndpointUrl:process.env.GRAPHQL_ENDPOINT,
