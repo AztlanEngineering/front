a2b6aa0f5c37f9c73f8467835f968e7842dc9a50
advl <700@779.mx>
2024-02-03 17:20:28 +0100

    withPreloadedQuery

---
+++ packages/react-relay/src/withPreloadedQuery.ts
@@ -0,0 +1,54 @@
+// withPreloadedQuery.tsx
+import React, { ElementType } from 'react'
+import {
+  usePreloadedQuery,
+  GraphQLTaggedNode,
+  PreloadedQuery,
+} from 'react-relay/hooks'
+
+/**
+ * Type definitions for the HOC inputs
+ */
+interface WithPreloadedQueryOptions {
+  WrappedComponent:ElementType;
+  QUERY           :GraphQLTaggedNode;
+}
+
+/**
+ * Type definition for the props expected by the HOC returned component
+ */
+interface WithPreloadedQueryProps {
+  queryRef     :PreloadedQuery<any>;
+  [key: string]:any; // Additional props
+}
+
+/**
+ * A Higher-Order Component (HOC) that provides preloaded query data to the wrapped component.
+ *
+ * @param {ElementType} WrappedComponent - The React component to wrap.
+ * @param {GraphQLTaggedNode} QUERY - The GraphQL query.
+ * @returns A new component that fetches data and renders the WrappedComponent with that data.
+ */
+function withPreloadedQuery({
+  WrappedComponent,
+  QUERY,
+}: WithPreloadedQueryOptions) {
+  return function ComponentWithPreloadedQuery(props: WithPreloadedQueryProps) {
+    const {
+      queryRef, ...otherProps
+    } = props
+    const data = usePreloadedQuery(
+      QUERY, queryRef,
+    )
+
+    // Use React.createElement instead of JSX
+    return React.createElement(
+      WrappedComponent, {
+        data,
+        ...otherProps,
+      },
+    )
+  }
+}
+
+export default withPreloadedQuery
