ab0f71e5a7c2b269d40b90281f4cfbf501bffd43
advl <700@779.mx>
2024-02-17 23:56:59 +0100

    Namespaces exports of nav

--- packages/ui/src/ui/common/Navigation/index.ts
+++ packages/ui/src/ui/common/Navigation/index.ts
@@ -1,4 +1,9 @@
 /* @aztlan/generator-front 1.1.4 */
 export { default as Navigation } from './Navigation.js'
 export { default as SequentialNavigation } from './SequentialNavigation.js'
-export * from './common/index.js'
+export {
+  Header as NavigationHeader,
+  Footer as NavigationFooter,
+  VerticalMenu as NavigationVerticalMenu,
+  Paginator as NavigationPaginator,
+} from './common/index.js'
