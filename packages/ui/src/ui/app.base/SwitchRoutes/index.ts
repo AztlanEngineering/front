a2c3be17b6f20131f9706fda5b427c84c1ffffd3
advl <700@779.mx>
2024-02-24 01:26:52 +0100

    getPaths

--- packages/ui/src/ui/app.base/SwitchRoutes/index.ts
+++ packages/ui/src/ui/app.base/SwitchRoutes/index.ts
@@ -1,4 +1,5 @@
 /* @aztlan/generator-front 0.4.0 */
 export { default as SwitchRoutes } from './SwitchRoutes.js'
 export { default as PrivateRoute } from './PrivateRoute.js'
+export { default as getPaths } from './getRoutes.js'
 export { default as getRoutes } from './getRoutes.js'
