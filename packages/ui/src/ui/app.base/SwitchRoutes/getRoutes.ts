9d58f071a7eb76c221f473df2bd66402678a4c91
advl <700@779.mx>
2024-02-24 01:18:40 +0100

    getPaths

---
+++ packages/ui/src/ui/app.base/SwitchRoutes/getRoutes.ts
@@ -0,0 +1,35 @@
+type RelativePaths = {
+  [key: string]:string;
+}
+
+interface Paths {
+  absolute     :Record<string, string>;
+  all          :string[];
+  relativePaths:RelativePaths;
+}
+
+export default function getPaths(
+  basePath: string,
+  relativePaths: RelativePaths,
+): Paths {
+  const result = Object.keys(relativePaths).reduce(
+    (
+      acc: { absolute: Record<string, string>; all: string[] }, key: string,
+    ) => {
+      const absolutePath = basePath + relativePaths[key]
+      acc.absolute[key] = absolutePath // Store the absolute path
+      acc.all.push(absolutePath) // Collect all absolute paths
+      return acc
+    },
+    {
+      absolute:{},
+      all     :[],
+    },
+  )
+
+  // Return the result along with the original relativePaths
+  return {
+    ...result,
+    relativePaths,
+  }
+}
--- packages/ui/src/ui/app.base/SwitchRoutes/index.ts
+++ packages/ui/src/ui/app.base/SwitchRoutes/index.ts
@@ -1,4 +1,4 @@
 /* @aztlan/generator-front 0.4.0 */
-export {
-  default as SwitchRoutes,
-} from './SwitchRoutes.js'
+export { default as SwitchRoutes } from './SwitchRoutes.js'
+export { default as PrivateRoute } from './PrivateRoute.js'
+export { default as getRoutes } from './getRoutes.js'
