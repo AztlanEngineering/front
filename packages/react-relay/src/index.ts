48b923eab31359066a404a4139f90375ad6180cc
advl <700@779.mx>
2024-03-02 01:23:49 +0100

    Missing export

--- packages/react-relay/src/index.ts
+++ packages/react-relay/src/index.ts
@@ -1,4 +1,5 @@
 export * from './authentication/index.js'
+export * from './hooks/index.js'
 export { default as withPreloadedQuery } from './withPreloadedQuery.js'
 export {
   decodeGlobalId, encodeGlobalId,
